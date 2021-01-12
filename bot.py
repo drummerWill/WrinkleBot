eba814a033fb497265f972aee36b60e8c85e1df4
drummerWill <53499870+drummerWill@users.noreply.github.com>
2021-01-11 19:09:17 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,20 +14,32 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
-
-    if message.content.startswith('$plot'):
-        msg = r.get('foo')
-        await message.channel.send(msg)
+    
+    if message.content.startswith('*smoothlist'):
+        await message.channel.send('')
         return
 
-    # if message.content.startswith('$skrap'):
-    #     newMsg = rr(message.content)
-    #     await message.channel.send(newMsg)
-    #     return
+    if message.content.startswith('*wrinklelist'):
+        await message.channel.send('')
+        return
 
-async def run(originalMessage):
-    print('')
+    if message.content.startswith('*smooth'):
+        members = message.mentions[0]
+        await message.channel.send('Gave ' + members.name + ' a smooth.')
+        return
 
+    if message.content.startswith('*wrinkle'):
+        members = message.mentions[0]
+        hasEntry = r.exists(members.name)
+        data = {'wrinkles':0, 'smooths':0}
+        if hasEntry == True:
+            data = eval(r.get(members.name).decode("utf-8"))
+        
+        data['wrinkles'] = data['wrinkles'] + 1
+
+        r.set(members.name, data)
+        await message.channel.send('Gave ' + members.name + ' a wrinkle.')
+        return
 
 
 client.run(token) 
