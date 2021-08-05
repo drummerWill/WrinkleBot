887501f5540da22737329532b8ca9c75da4f7025
drummerWill <53499870+drummerWill@users.noreply.github.com>
2021-08-04 20:19:38 -0500

    Update gacha.py

--- gacha.py
+++ gacha.py
@@ -1,14 +1,28 @@
-gachas = {}
-gachas[1] =  {'name': 'William', 'link': 'kdfjgddfgjkkjdfg'}
-gachas[2] =  {'name': 'Dean', 'link': 'dfgkjh345dfgkjh'}
+import random
 
+
+gachas = {}
+gachas['1a'] =  {'name': 'William', 'link': 'kdfjgddfgjkkjdfg'}
+gachas['2a'] =  {'name': 'Dean', 'link': 'dfgkjh345dfgkjh'}
 
 def roll(user, userdata):
-    gacha = userdata['gacha']
-    gachalist = gacha['gachalist']
-    reward = 'you did it'
-    gachaid = 1
-    userdata['gacha']['gachalist'].append({'amount': 1, 'id':gachaid})
+    # gacha = userdata['gacha']
+    # gachalist = gacha['gachalist']
+    
+
+    #find the correct dictionary to look for 
+
+
+    gachaid = random.choice(list(gachas.keys()))
+    recievedGacha = gachas[gachaid]
+    reward = 'you got ' + recievedGacha['name']
+
+    amount = 1
+    if (next((item for item in userdata['gacha']['gachalist'] if item["id"] == gachaid), None)):
+        i = next((i for i, item in enumerate(userdata['gacha']['gachalist']) if item["id"] == gachaid))
+        amount = userdata['gacha']['gachalist'][i]['amount'] + 1
+
+    userdata['gacha']['gachalist'].append({'amount': amount, 'id':gachaid})
     # for card in gachalist:
     #     amount = card['amount']
     #     gachaItem = gachas[card['id']]
