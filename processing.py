631b6d938357f11c4749ac36d93f67297aae5fc6
drummerWill <53499870+drummerWill@users.noreply.github.com>
2022-01-06 19:24:12 -0600

    Update processing.py

--- processing.py
+++ processing.py
@@ -8,6 +8,6 @@ def runProcessing(myDict, name, option):
 def countEmoji(channel, name):
     for user in channel.items():
         for msg in user[1]:
-            emojis = re.findall(r'(<a?)?:\w+:(\d{18}>)', msg.content)
+            emojis =  [x.group() for x in re.finditer(r'(<a?)?:\w+:(\d{18}>)', msg.content)]
             print(emojis)
     return ""
